f8587b9a4d032c9f669232de7e9235ed77a94228
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2023-11-04 21:40:42 -0700

    create DOM object to hold DOM selectors

---
+++ HangMan.js
@@ -0,0 +1,8 @@
+const DOM = {
+    button : document.getElementById("button"),
+    letterBank : document.getElementById("letter-bank"),
+    alphabet : document.getElementById("alphabet"),
+    start : document.getElementById("start"),
+    restart : document.getElementById("restart"),
+    input: document.getElementById("user-answer")
+}
