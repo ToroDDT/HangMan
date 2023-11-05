00421b09974de991fd1cf5123b3df7d3c13135c8
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2023-11-04 21:50:07 -0700

    get all indexes that match
    takes an array, compares each index with value, returns indexes

--- HangMan.js
+++ HangMan.js
@@ -39,3 +39,11 @@ function compareLetters () {
         }
         removeItem()
     }
+
+    function getAllIndexes(arr, val) {
+        var indexes = [], i = -1;
+        while ((i = arr.indexOf(val, i+1)) != -1){
+            indexes.push(i);
+        }
+        return indexes;
+    }
